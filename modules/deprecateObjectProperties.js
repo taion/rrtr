10ed1e82fcb324fd4d0ca6665b487d29d49d88d2
Jimmy Jia <tesrin@gmail.com>
2016-04-13 12:27:58 -0400

    Go back to membrane for deprecateObjectProperties (#18)

--- modules/deprecateObjectProperties.js
+++ modules/deprecateObjectProperties.js
@@ -4,23 +4,53 @@ import warning from './routerWarning'
 let deprecateObjectProperties = object => object
 
 if (__DEV__) {
-  if (typeof Proxy === 'function') {
+  let useMembrane = false
+
+  try {
+    if (Object.defineProperty({}, 'x', { get() { return true } }).x) {
+      useMembrane = true
+    }
+  /* eslint-disable no-empty */
+  } catch(e) {}
+  /* eslint-enable no-empty */
+
+  if (useMembrane) {
     deprecateObjectProperties = (object, message) => {
-      return new Proxy(object, {
-        get(target, name) {
-          // Only warn for own properties to avoid false positives.
-          if (Object.prototype.hasOwnProperty.call(target, name)) {
+      // Wrap the deprecated object in a membrane to warn on property access.
+      const membrane = {}
+
+      for (const prop in object) {
+        if (!Object.prototype.hasOwnProperty.call(object, prop)) {
+          continue
+        }
+
+        if (typeof object[prop] === 'function') {
+          // Can't use fat arrow here because of use of arguments below.
+          membrane[prop] = function () {
             warning(false, message)
+            return object[prop].apply(object, arguments)
           }
+          continue
+        }
 
-          return target[name]
-        },
+        // These properties are non-enumerable to prevent React dev tools from
+        // seeing them and causing spurious warnings when accessing them. In
+        // principle this could be done with a proxy, but support for the
+        // ownKeys trap on proxies is not universal, even among browsers that
+        // otherwise support proxies.
+        Object.defineProperty(membrane, prop, {
+          configurable: false,
+          enumerable: false,
+          get() {
+            warning(false, message)
+            return object[prop]
+          }
+        })
+      }
 
-        ownKeys: () => []
-      })
+      return membrane
     }
   }
 }
 
-// wraps an object in a membrane to warn about deprecated property access
 export default deprecateObjectProperties
